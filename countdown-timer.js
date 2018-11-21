b6149b249ab4c4e3c3ad9d661976445d500a943d
Kyle Jacobson <kjacobson@hearsaycorp.com>
2018-11-21 12:13:56 -0800

    move countdown timer to its own module

---
+++ countdown-timer.js
@@ -0,0 +1,64 @@
+class CountdownTimer {
+    constructor(duration, EventEmitter) {
+        this.duration = duration;
+        this.remaining = duration;
+        this.running = false;
+        this.emitter = new EventEmitter();
+        this._tick = this._tick.bind(this);
+        return this;
+    }
+    tick() {
+        if (this.remaining % 5 === 0) {
+            console.log(this.formattedTime());
+        }
+        setTimeout(this._tick, 1000);
+        return this;
+    }
+    _tick() {
+        if (this.running) {
+            this.remaining--;
+            if (this.remaining) {
+                this.tick();
+            } else {
+                this.running = false;
+                this.emitter.emit('zero');
+            }
+        }
+        return this;
+    }
+    start() {
+        this.running = true;
+        this.tick();
+        return this;
+    }
+    pause () {
+        this.running = false;
+        return this;
+    }
+    clear () {
+        this.running = false;
+        this.remaining = this.duration;
+        return this;
+    }
+    changeDuration (duration) {
+        if (!this.running) {
+            this.duration = duration;
+            this.clear();
+        }
+        return this;
+    }
+    formattedTime () {
+        const showMinutes = this.duration / 60 > 1;
+        const minutes = this.remaining / 60;
+        const minutesInt = Math.floor(minutes);
+        let seconds = this.remaining % 60;
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        return showMinutes || minutesInt ?
+            minutesInt + ':' + seconds :
+            seconds + '';
+    }
+}
+
+module.exports = CountdownTimer;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const Promise = require('es6-promise');
 const EventEmitter = require('events');
-const fetch = require('isomorphic-fetch');
+
+const CountdownTimer = require('./countdown-timer');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -86,69 +89,6 @@ const storeNGramData = (data) => {
     nGramData.count = nGramData.arr.length;
 };
 
-class CountdownTimer {
-    constructor(duration) {
-        this.duration = duration;
-        this.remaining = duration;
-        this.running = false;
-        this.emitter = new EventEmitter();
-        this._tick = this._tick.bind(this);
-        return this;
-    }
-    tick() {
-        if (this.remaining % 5 === 0) {
-            console.log(this.formattedTime());
-        }
-        setTimeout(this._tick, 1000);
-        return this;
-    }
-    _tick() {
-        if (this.running) {
-            this.remaining--;
-            if (this.remaining) {
-                this.tick();
-            } else {
-                this.running = false;
-                this.emitter.emit('zero');
-            }
-        }
-        return this;
-    }
-    start() {
-        this.running = true;
-        this.tick();
-        return this;
-    }
-    pause () {
-        this.running = false;
-        return this;
-    }
-    clear () {
-        this.running = false;
-        this.remaining = this.duration;
-        return this;
-    }
-    changeDuration (duration) {
-        if (!this.running) {
-            this.duration = duration;
-            this.clear();
-        }
-        return this;
-    }
-    formattedTime () {
-        const showMinutes = this.duration / 60 > 1;
-        const minutes = this.remaining / 60;
-        const minutesInt = Math.floor(minutes);
-        let seconds = this.remaining % 60;
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        return showMinutes || minutesInt ?
-            minutesInt + ':' + seconds :
-            seconds + '';
-    }
-}
-
 const loadNGramData = () => {
     return new Promise((resolve, reject) => {
         fs.readFile(
@@ -174,7 +114,7 @@ const randomNGram = () => {
 };
 
 loadNGramData().then((data) => {
-    timer = new CountdownTimer(appSettings.time)
+    timer = new CountdownTimer(appSettings.time, EventEmitter);
     timer.emitter.on('zero', () => {
         loseRound();
     });
